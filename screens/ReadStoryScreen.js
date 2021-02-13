b7402c8341dec4aac4c5e65b5f52e2b2aa6d0a92
avs1325 <61194716+avs1325@users.noreply.github.com>
2021-02-13 14:59:21 +0530

    Add files via upload

---
+++ screens/ReadStoryScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, Image, TextInput, Alert,} from 'react-native';
+
+export default class ReadStoryScreen extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>
+          baba
+        </Text>
+      </View>
+    )
+  }
+}
